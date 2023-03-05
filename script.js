5e8e3efd22b3772c147cc6e563aa89a54381bf28
PratikshaPadole <123239895+PratikshaPadole@users.noreply.github.com>
2023-03-06 01:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,242 @@
+let products ={
+    data:[{
+        productName:"Red long onepeice",
+        category:"Onepiece",
+        price:"100",
+        image:" https://rukminim1.flixcart.com/image/832/832/kr9jafk0/gown/f/o/s/na-xs-sleeveless-stitched-leg-red-xs1-starword-na-original-imag53mp6yphhgxf.jpeg?q=70  " ,},
+      {
+        productName:"Green paithni saree",
+        category:"Saree",
+        price:"500",
+        image:"https://cdn.shopify.com/s/files/1/0582/4492/3589/products/RJMEERA-1_1000x.jpg?v=1639650082",
+      }
+      ,
+    {
+        productName:"Kurti",
+        category:"Kurti",
+        price: "300",
+        image:  "https://image.winudf.com/v2/image/Y29tLmZhc2hpb24ua3VydGlkZXNpZ25fc2NyZWVuXzBfMTUzOTI2Njk1MV8wMjY/screen-0.jpg?fakeurl=1&type=.webp",
+    },
+    {
+        productName:"Black jacket",
+        category:"Jacket",
+        price:"500",
+        image:"https://static.wixstatic.com/media/524c26_c04dec48f6e440fcadd62fe6956cc35a~mv2.jpg/v1/fill/w_746,h_994,al_c,q_85,usm_0.66_1.00_0.01/524c26_c04dec48f6e440fcadd62fe6956cc35a~mv2.jpg",
+    }
+    ,
+      { productName:"Fancy white saree",
+        category:"Saree",
+        price:"50",
+        image: "https://cdn.shopify.com/s/files/1/1105/1322/products/Dutch_White_Designer_Embroidered_Fancy_Party_Wear_Saree_2_-_Saira_s_Boutique.png?v=1568869787",
+}
+  ,  {
+        productName:"white jacket",
+        category:"Jacket",
+        price:"450",
+        image: "https://5.imimg.com/data5/HD/EC/IB/ANDROID-54157926/product-jpeg-500x500.jpg",
+    }
+    ,
+    {
+        productName:"red fancy kurti",
+        category:"Kurti",
+        price:"300",
+        image: "https://i.pinimg.com/474x/7f/97/5a/7f975a813a092c7620441e0a841cd12c.jpg",
+    },
+    {
+        productName:"stylish jacket army color",
+        category:"Jacket",
+        price:"800",
+        image: "https://3.imimg.com/data3/AU/KI/MY-646899/men-s-jackets-500x500.jpg",
+    },
+    {
+        productName:"long yellow kurti",
+        category:"kurti",
+        price:"600",
+        image: "https://www.vpvilla.in/wp-content/uploads/2021/09/vp-01-177.jpg",
+    },
+    {
+        productName:"Anarkali kurti",
+        category:"kurti",
+        price:"300",
+        image: "https://images.meesho.com/images/products/82996035/usia0_512.jpg",
+    },
+    {
+        productName:"White Onepiece",
+        category:"Onepiece",
+        price:"350",
+        image: "https://static.theprint.in/wp-content/uploads/2022/06/289320748_174007811731071_4445764222703744726_n2022062216485420220622165533.jpg",
+    },
+    {
+        productName:"shifon saree",
+        category:"saree",
+        price:"900",
+        image: "https://assets.ajio.com/medias/sys_master/root/20220815/JduG/62f9fd64aeb26921aff03cf3/-473Wx593H-464490905-purple-MODEL.jpg",
+    },
+    {
+        productName:"printed jacket",
+        category:"Jacket",
+        price:"300",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdMZ79cTJclTovWs_xj7MWyp2x0ukDFqkaWQ&usqp=CAU",
+    },
+    {
+        productName:"Pink fullsleeves Onepiece",
+        category: "Onepiece",
+        price:"300",
+        image: "https://images.hindustantimes.com/img/2022/12/16/1600x900/photo_2022-12-16_06-54-16_1671203717720_1671203789725_1671203789725.jpg",
+    },
+    {
+        productName:"Printed Onepiece",
+        category:"Onepiece",
+        price:"300",
+        image: "printed pic.jpg",
+    },
+    {
+        productName:"Orange bodycone dress",
+        category: "Onepiece" ,
+        price:"500",
+        image: "https://static.india.com/wp-content/uploads/2022/05/WhatsApp-Image-2022-05-24-at-1.56.05-PM.jpeg",
+    },
+    {
+        productName:"Navy blue saree",
+        category:"Saree",
+        price:"700",
+        image: "https://cdn.shopify.com/s/files/1/1760/4649/products/south-silk-saree-navy-blue-south-silk-saree-silk-saree-online-32838121455809_grande.jpg?v=1660993871",
+    },
+    {
+        productName:"Red saree for wedding",
+        category:"Saree",
+        price:"1000",
+        image: "https://i.pinimg.com/736x/83/dd/c3/83ddc3c278519992e1bf6db7e53284b8.jpg",
+    },
+    {
+        productName:"Softnet pink designer saree",
+        category:"Saree",
+        price:"500",
+        image: "https://mallurepost.com/upload/2021/08/08/Shraddha-Arya-pink-saree-Photos-Shraddha-Arya-latest-hot-and-spicy-Photos-11484.jpg",
+    },
+    {
+        productName:" Redish Jacket",
+        category:"Jacket",
+        price:"900",
+        image: "https://www.shutterstock.com/image-photo/young-attractive-man-sunglasses-red-260nw-1656026950.jpg",
+    },
+    {
+        productName:"Flower print kurti",
+        category:"Kurti",
+        price:"48",
+        image: "https://cdn.shopify.com/s/files/1/0601/9760/1513/products/512-_3_1200x.jpg?v=1643107272",
+    },
+    {
+        productName:"black printed kurti",
+        category:"Kurti",
+        price:"300",
+        image: "https://cdn.shopify.com/s/files/1/0601/9760/1513/products/1n-_12_320x425.jpg?v=1643263745",
+    },
+    {
+        productName:"black Onepiece",
+        category:"Onepiece",
+        price:"300",
+        image: "https://cdn.shopify.com/s/files/1/0601/9760/1513/products/4_1e578f25-7084-40e2-99ea-efa8dd255690_1200x.jpg?v=1634900633",
+    },
+],
+};
+for(let i of products.data){
+    //create card
+    let  card = document.createElement("div");
+   
+    //card should have category
+    card.classList.add("card", i.category ,"hide");
+    //image div
+
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+
+    let container =document.createElement("div");
+    container.classList.add("container");
+
+    //product name
+
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    
+    //price
+
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+    
+
+}
+// parameter passed from button(parameter same as category )
+function filterproduct(value){
+    //button class code 
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        // check if value  equals to innerText 
+        if(value.toUpperCase() == button.innerText.toUpperCase()){
+            button.classList.add("active");
+        }
+        else{
+            button.classList.remove("active");
+        }
+    });
+    // select all cards
+
+    let elements = document.querySelectorAll(".card");
+    //loop through all the cards
+    elements.forEach((element) => {
+        if (value == "all"){
+            element.classList.remove("hide");
+        }
+        else{
+            // check if elements contains category class
+            if(element.classList.contains(value)){
+                //display element based on category
+                element.classList.remove("hide");
+            }
+            else{
+                //hide  other elements
+                element.classList.add("hide");
+            }
+        }
+    });
+}
+// search button click
+document.getElementById("search").addEventListener
+("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    
+
+    //loop through all elements
+    elements.forEach((element,index) => {
+        //check if text includes the search value
+        if (element.innerText.includes(searchInput.toUpperCase())){
+            //display  matching cards
+            cards[index].classList.remove("hide");
+        }
+        else{
+            // hide others
+            cards[index].classList.add("hide");
+        }
+
+    })
+})
+// initially display all products
+window.onload =() => {
+    filterproduct("all");
+};
